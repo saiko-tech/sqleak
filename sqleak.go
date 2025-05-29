bc4c03c30208c8fc8ef975c1ddc607a502d60660
Markus Walther <mark@saiko.tech>
2025-05-29 17:47:01 +0100

    properly handle DriverContext case

--- sqleak.go
+++ sqleak.go
@@ -184,8 +184,48 @@ func (d *monitoredDriver) Open(name string) (driver.Conn, error) {
 	return mc, err
 }
 
+type monitoredConnector struct {
+	driver.Connector
+	driver *monitoredDriver
+}
+
+func (c *monitoredConnector) Connect(ctx context.Context) (driver.Conn, error) {
+	conn, err := c.Connector.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	mc := &monitoredConn{
+		Conn:    conn,
+		timeout: c.driver.timeout,
+	}
+
+	return mc, nil
+}
+
+func (c *monitoredConnector) Driver() driver.Driver {
+	return c.driver
+}
+
+func (c *monitoredConnector) Close() error {
+	// database/sql uses a type assertion to check if connectors implement io.Closer.
+	// The type assertion does not pass through to monitoredConnector.Connector, so we explicitly implement it here.
+	if closer, ok := c.Connector.(interface{ Close() error }); ok {
+		return closer.Close()
+	}
+	return nil
+}
+
 func (d *monitoredDriver) OpenConnector(name string) (driver.Connector, error) {
-	return d.driver.(driver.DriverContext).OpenConnector(name)
+	connector, err := d.driver.(driver.DriverContext).OpenConnector(name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &monitoredConnector{
+		Connector: connector,
+		driver:    d,
+	}, nil
 }
 
 type Option func(*monitoredDriver)
